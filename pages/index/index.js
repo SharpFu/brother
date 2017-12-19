a67642321d415fe8e813a1715b30959ed3180b8a
SharpFu <824314551@qq.com>
2017-12-19 15:05:55 +0800

    首页修改
    首页修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,32 +1,106 @@
-var Zan = require('../../common/index');
+var util = require('../../utils/util.js')
+var app = getApp()
 
-Page(Object.assign({}, Zan.Tab, {
+Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    tabItem: {
-      list: [{
-        id: '1',
-        title: '商品1'
-      }, {
-        id: '2',
-        title: '商品2'
-      }, {
-        id: '3',
-        title: '商品3'
-      }, {
-        id: '4',
-        title: '商品4'
-      }],
-      selectedId: '1',
-      scroll: true,
-      height: 45
-    }
+    news:[],
+    images:[],
+    news_length:0,
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    loadingHidden: false
   },
 
-  handleZanTabChange(e) {
-    var componentId = e.componentId;
-    var selectedId = e.selectedId;
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getSwiperImages();
+    this.getAllData();
+  },
+  //事件处理函数
+  swiperchange: function (e) {
+    //console.log(e.detail.current)
+  },
+  getSwiperImages(){
+    //sliderList
+    var _images = util.getImages();
+    this.setData({images:_images});
+    // wx.request({
+    //   url: 'http://huanqiuxiaozhen.com/wemall/slider/list',
+    //   method: 'GET',
+    //   data: {},
+    //   header: {
+    //     'Accept': 'application/json'
+    //   },
+    //   success: function (res) {
+    //     that.setData({
+    //       images: res.data
+    //     })
+    //   }
+    // })
+  },
+  getAllData(){
+    var _newsData = util.getIndexData();
+    var _news = _newsData.data;
     this.setData({
-      [`${componentId}.selectedId`]: selectedId
-    });
+      news: _news,
+      news_length:_news.length
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
-}));
+})
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,74 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
 
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
-const formatNumber = n => {
+function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
 module.exports = {
   formatTime: formatTime
+};
+
+var index = require('../data/indexData.js')
+var index_next = require('../data/indexData.js')
+var discovery = require('../data/indexData.js')
+var discovery_next = require('../data/indexData.js')
+
+function getData(url) {
+  return new Promise(function (resolve, reject) {
+    wx.request({
+      url: url,
+      data: {},
+      header: {
+        //'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log("success")
+        resolve(res)
+      },
+      fail: function (res) {
+        reject(res)
+        console.log("failed")
+      }
+    })
+  })
 }
+
+function getIndexData() {
+  return index.index;
+}
+
+function getNext() {
+  return index_next.next;
+}
+
+function getDiscovery() {
+  return discovery.discovery;
+}
+
+function discoveryNext() {
+  return discovery_next.next;
+}
+function getImages(){
+  return index.swiperImages;
+}
+
+module.exports.getImages = getImages;
+
+
+module.exports.getData = getData;
+module.exports.getIndexData = getIndexData;
+module.exports.getNext = getNext;
+module.exports.getDiscovery = getDiscovery;
+module.exports.discoveryNext = discoveryNext;
